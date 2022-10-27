447a618f4dbcd8cf7990ea7ab1ffba6ced663d19
Fernando AG <111654890+Fernando43141@users.noreply.github.com>
2022-10-26 19:03:05 -0500

    Add files via upload

---
+++ Parcial 2/Ejercicio 1_Con_Clases/Vehiculo.dart
@@ -0,0 +1,64 @@
+void main() {
+  Vehiculo conductor =
+      Vehiculo(No_llantas: 4, marca: "Jeep", modelo: 2003, color: "Blanco");
+  conductor.encender();
+  conductor.frenar();
+  conductor.estacionar();
+  print(conductor.getLlantas);
+  print(conductor.getmarca);
+  print(conductor.getmodelo);
+  print(conductor.getcolor);
+}
+
+class Vehiculo {
+  int? No_llantas;
+  String? marca;
+  int? modelo;
+  String? color;
+
+  void estacionar() {
+    print("Se estaciono");
+  }
+
+  void frenar() {
+    print("Freno");
+  }
+
+  void encender() {
+    print("Se encendio");
+  }
+
+  Vehiculo({this.No_llantas, this.marca, this.modelo, this.color});
+
+  void set setLlantas(int setLlantas) {
+    No_llantas = setLlantas;
+  }
+
+  int get getLlantas {
+    return No_llantas!;
+  }
+
+  void set setMarca(String setMarca) {
+    marca = setMarca;
+  }
+
+  String get getmarca {
+    return marca!;
+  }
+
+  void set setModelo(int setModelo) {
+    modelo = setModelo;
+  }
+
+  int get getmodelo {
+    return modelo!;
+  }
+
+  void set setColor(String setColor) {
+    color = setColor;
+  }
+
+  String get getcolor {
+    return color!;
+  }
+}
