644d9f285be1bb5b8b3a66f0373cad5b430633ee
Fernando AG <111654890+Fernando43141@users.noreply.github.com>
2022-12-04 10:22:07 -0600

    Add files via upload

---
+++ Parcial 3/4.-__Invocaciones internas de una funcion[Geometria(perimetro_cuadrado)]/geometria.ex
@@ -0,0 +1,8 @@
+#<-----Modulo geometria----->
+#Invocaciones internas
+defmodule Geometria do
+  def perimetro1(l), do: cuadrado(l)
+  def perimetro2(l), do: Geometria.cuadrado(l)
+  def cuadrado(l), do: 4*l
+end
+#<-----Modulo geometria----->
