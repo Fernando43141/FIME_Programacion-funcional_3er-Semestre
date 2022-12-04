d6651d302bf2fc539fe9e5277f2479ae006a5f33
Fernando AG <111654890+Fernando43141@users.noreply.github.com>
2022-12-04 10:45:31 -0600

    Add files via upload

---
+++ Parcial 3/16.-__Proyecto-Mix/lib/calculadora.ex
@@ -0,0 +1,18 @@
+defmodule Calculadora do
+  @moduledoc """
+  Documentation for `Calculadora`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calculadora.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
