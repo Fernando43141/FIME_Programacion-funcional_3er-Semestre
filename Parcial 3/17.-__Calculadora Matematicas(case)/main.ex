ad8a9702b4c76dd8a7fb1a6c29f64a836fe9f06b
Fernando AG <111654890+Fernando43141@users.noreply.github.com>
2022-12-04 10:53:16 -0600

    Add files via upload

---
+++ Parcial 3/17.-__Calculadora Matematicas(case)/main.ex
@@ -0,0 +1,18 @@
+defmodule Matematicas do
+  def calculadora(opcion,{n1,n2}) do
+    case opcion do
+      "+" -> n1+n2
+      "-" -> n1-n2
+      "/" when n2 != 0 -> n1/n2
+      "/" when n2 == 0 -> "No se puede dividir por 0"
+      "*" -> n1*n2
+      _ -> :error
+    end
+  end
+end
+
+IO.inspect Matematicas.calculadora("+",{5,4})
+IO.inspect Matematicas.calculadora("-",{5,4})
+IO.inspect Matematicas.calculadora("/",{5,4})
+IO.inspect Matematicas.calculadora("/",{5,0})
+IO.inspect Matematicas.calculadora("*",{5,4})
